244e81b90595e177c1ad781f37151514d65bcd20
Flamerinus <alexmosuyevon@yahoo.es>
2018-01-06 07:26:21 +0100

    Updated p5 Language function done

--- LANG.js
+++ LANG.js
@@ -10,8 +10,41 @@ var eng = {
   e2:"Helium",
   e3:"Lithium",
   e4:"Beryllium",
+
 };
 
-function goesp();{
+function goeng(){
+  var x = document.getElementsByClassName("element");
+  var y = Object.keys(eng);
+  var z = Object.values(eng);
+  for(var i = 0; i < x.length; i++) {
+    for(var j = 0; j < y.length; j++){
+      if(x[i].id == y[j]){
+      document.getElementById(x[i].id).innerHTML = z[j];
+      }
+    }
+
+  }
+  //alert("total " + z.length + " keys: " +z + y);
+}
+
+function goesp(){
+  var x = document.getElementsByClassName("element");
+  var y = Object.keys(esp);
+  var z = Object.values(esp);
+  for(var i = 0; i < x.length; i++) {
+    for(var j = 0; j < y.length; j++){
+      if(x[i].id == y[j]){
+      document.getElementById(x[i].id).innerHTML = z[j];
+      }
+    }
+
+  }
+  //alert("total " + z.length + " keys: " +z + y);
+}
+
+function prueba(){
+
+
 
 }
